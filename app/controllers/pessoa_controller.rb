63f336f4789867e4bc306af1fe12877933dc4879
Helena Freire <helenamcfreire@gmail.com>
2013-08-16 15:18:20 -0300

    Criando um método para associar participantes a um chat

--- app/controllers/pessoa_controller.rb
+++ app/controllers/pessoa_controller.rb
@@ -22,7 +22,31 @@ class PessoaController < ApplicationController
 
   def add_pessoas_chat
 
+    #o método eval() serve pra transformar uma string em array
+    ids_participantes = eval(params[:idsParticipantes])
 
+    id_chat = next_chat_id()
+
+    ids_participantes.each do |id_participante|
+      @pessoa_chat = PessoaChat.new
+      @pessoa_chat.pessoa_id = id_participante
+      @pessoa_chat.chat_id = id_chat
+      @pessoa_chat.save
+    end
+
+  end
+
+  def next_chat_id
+
+    id_chat = 1
+
+    ultimo_chat = PessoaChat.order(:chat_id).last
+
+    if ultimo_chat != nil
+      id_chat = ultimo_chat.chat_id.next
+    end
+
+    id_chat
   end
 
   def list_chats
