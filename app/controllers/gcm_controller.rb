86a1818bceaa5a29d2d5381bc228a03b044f2f4f
Helena Freire <helenamcfreire@gmail.com>
2014-02-12 00:38:39 -0200

    GCM

--- app/controllers/gcm_controller.rb
+++ app/controllers/gcm_controller.rb
@@ -4,7 +4,8 @@ class GcmController < ApplicationController
     gcm_api_key = 'AIzaSyBv8OPZapshhVIQWtowJcKGp_VV0waMv94'
     gcm = GCM.new(gcm_api_key)
     registration_ids = [params[:regId]]
-    gcm.send_notification(registration_ids)
+    #gcm.send_notification(registration_ids)
+    gcm.send_notification({registration_ids: registration_ids, data: {score: 'teste'}})
   end
 
 end
